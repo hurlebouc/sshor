8dc7228911d7951a5f139c71dfa596f76ef0f770
hubert <hubert@hubert.com>
2024-07-05 09:07:24 +0200

    nix buildable

--- build.nix
+++ build.nix
@@ -4,7 +4,7 @@ buildGoModule {
   pname = "sshor-go";
   version = "0.1.0";
 
-  src = nix-gitignore.gitignoreSource [".git" ".gitignore" "*.nix"] ./.;
+  src = nix-gitignore.gitignoreSource [ ".git" ".gitignore" "*.nix" ] ./.;
 
-  vendorHash = null; # get hash after first build
+  vendorHash = "sha256-IdjZIoI9IL1IpN5SlK/SUrIN3vLFW/D65ZA+ODBAoqg="; # get hash after first build
 }
