0c185079fb5a98316599cb9bd8ad43de90b29d73
hubert <hubert@hubert.com>
2024-07-06 23:53:09 +0200

    save

--- ssh/shell.go
+++ ssh/shell.go
@@ -1,6 +1,7 @@
 package ssh
 
 import (
+	"bytes"
 	"context"
 	"os"
 	"os/user"
@@ -9,6 +10,26 @@ import (
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/term"
 )
+
+type PatternDetector struct {
+	buffer   []byte
+	position int // next byte to write
+}
+
+func (p *PatternDetector) addBytes(b []byte) {
+	for i, v := range b {
+		p.buffer[(i+p.position)%len(p.buffer)] = v
+	}
+	p.position = (p.position + len(b)) % len(p.buffer)
+}
+
+func (p PatternDetector) exportBytes() []byte {
+	res := make([]byte, len(p.buffer))
+	for i := range res {
+		res[i] = p.buffer[(i+p.position)%len(p.buffer)]
+	}
+	return res
+}
 
 func Shell(hostConf config.Host, passwordFlag, keepassPwdFlag string) {
 	keepassPwdMap := map[string]string{}
@@ -52,9 +73,69 @@ func Shell(hostConf config.Host, passwordFlag, keepassPwdFlag string) {
 		panic(err)
 	}
 
-	session.Stdin = os.Stdin
-	session.Stdout = os.Stdout
+	//session.Stdin = os.Stdin
+	input, err := session.StdinPipe()
+	if err != nil {
+		panic(err)
+	}
+	//session.Stdout = os.Stdout
+	output, err := session.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	cha := make(chan bool)
 	session.Stderr = os.Stderr
+
+	go func() {
+		input.Write([]byte("su - testuser\n"))
+		b := <-cha
+		//time.Sleep(1 * time.Second)
+		if b {
+			input.Write([]byte("testuser\n"))
+		}
+		buffer := make([]byte, 5)
+		for {
+			n, err := os.Stdin.Read(buffer)
+			if n == 0 {
+				break
+			}
+			if err != nil {
+				panic(err)
+			}
+			_, err = input.Write(buffer[0:n])
+			if err != nil {
+				panic(err)
+			}
+		}
+	}()
+
+	go func() {
+		buffer := make([]byte, 5)
+		p := PatternDetector{
+			buffer: make([]byte, 100),
+		}
+		passed := false
+		for {
+			n, err := output.Read(buffer)
+			if n == 0 {
+				break
+			}
+			if err != nil {
+				panic(err)
+			}
+			if !passed {
+				p.addBytes(buffer[0:n])
+				if bytes.Contains(p.exportBytes(), []byte("Mot de passe : ")) {
+					cha <- true
+					passed = true
+				}
+			}
+			_, err = os.Stdout.Write(buffer[0:n])
+			if err != nil {
+				panic(err)
+			}
+		}
+	}()
 
 	// Start remote shell
 	if err := session.Shell(); err != nil {
