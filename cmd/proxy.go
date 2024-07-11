c74b274d484e2923b0a8b94fcb85fcd63107ef7c
hubert <hubert@hubert.com>
2024-07-11 09:06:08 +0200

    save forward proxy

--- cmd/proxy.go
+++ cmd/proxy.go
@@ -4,8 +4,7 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/hurlebouc/sshor/ssh"
 	"github.com/spf13/cobra"
 )
 
@@ -14,8 +13,9 @@ var proxyForwardCmd = &cobra.Command{
 	Use:   "proxy-forward",
 	Short: "Forward connections from local to remote",
 	Long:  `This command opens a listening port on local host and forwards each request on this port to a destination accessible from the remote host.`,
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("proxy called")
+		ssh.ForwardProxy(getHostConfig(readConf(), args[0]), getOptions(), passwordFlag, keepassPwdFlag, proxyForwardOptions.listeningIP, proxyForwardOptions.listeningPort, proxyForwardOptions.destinationIP, proxyForwardOptions.destinationPort)
 	},
 }
 
@@ -28,7 +28,7 @@ var proxyForwardOptions struct {
 
 const LISTENING_IP = "listening-ip"
 const LISTENING_PORT = "listening-port"
-const DESTINATION_IP = "destination-ip"
+const DESTINATION_ADDR = "destination-addr"
 const DESTINATION_PORT = "destination-port"
 
 func init() {
@@ -36,7 +36,7 @@ func init() {
 
 	proxyForwardCmd.Flags().StringVar(&proxyForwardOptions.listeningIP, LISTENING_IP, "127.0.0.1", "local listening IP")
 	proxyForwardCmd.Flags().Uint16Var(&proxyForwardOptions.listeningPort, LISTENING_PORT, 0, "local listening port (default use random)")
-	proxyForwardCmd.Flags().StringVar(&proxyForwardOptions.destinationIP, DESTINATION_IP, "127.0.0.1", "destination IP accessible from remote host")
-	proxyForwardCmd.Flags().Uint16Var(&proxyForwardOptions.destinationPort, DESTINATION_PORT, 0, "destination IP accessible from remote host")
+	proxyForwardCmd.Flags().StringVar(&proxyForwardOptions.destinationIP, DESTINATION_ADDR, "127.0.0.1", "destination address accessible from remote host")
+	proxyForwardCmd.Flags().Uint16Var(&proxyForwardOptions.destinationPort, DESTINATION_PORT, 0, "destination port accessible from remote host")
 	proxyForwardCmd.MarkFlagRequired(DESTINATION_PORT)
 }
--- ssh/proxy.go
+++ ssh/proxy.go
@@ -1,7 +1,78 @@
 package ssh
 
-import "math/rand/v2"
+import (
+	"fmt"
+	"io"
+	"log"
+	"math/rand/v2"
+	"net"
+
+	"github.com/hurlebouc/sshor/config"
+)
 
 func randomPort() uint16 {
 	return uint16(rand.Uint32())
+}
+
+func ForwardProxy(hostConf config.Host, options Options, passwordFlag, keepassPwdFlag string, listeningIp string, listeningPort uint16, destinationAddr string, destinationPort uint16) {
+	keepassPwdMap := InitKeepassPwdMap(hostConf, keepassPwdFlag)
+	ctx := InitContext()
+	sshClient, _ := NewSshClient(ctx, hostConf, passwordFlag, keepassPwdMap)
+	defer sshClient.Close()
+	if sshClient.Client == nil {
+		log.Panicln("Cannot change user of proxied connection")
+	}
+
+	listPort := listeningPort
+	if listeningPort == 0 {
+		listPort = randomPort()
+	}
+
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", listeningIp, listPort))
+
+	if listeningPort == 0 {
+		for err != nil {
+			listener, err = net.Listen("tcp", fmt.Sprintf("%s:%d", listeningIp, listeningPort))
+		}
+	}
+
+	log.Printf("listening at %s:%d", listeningIp, listPort)
+
+	for {
+		localConn, err := listener.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+		if options.Verbose {
+			log.Printf("new connection from %s", localConn.LocalAddr().String())
+		}
+
+		go func() {
+			defer localConn.Close()
+
+			remoteConn, err := sshClient.Client.Dial("tcp", fmt.Sprintf("%s:%d", destinationAddr, destinationPort))
+			if err != nil {
+				panic(err)
+			}
+			defer remoteConn.Close()
+			forwardChan := make(chan bool)
+			go func() {
+				io.Copy(remoteConn, localConn)
+				close(forwardChan)
+			}()
+			backwardChan := make(chan bool)
+			go func() {
+				io.Copy(localConn, remoteConn)
+				close(backwardChan)
+			}()
+			for range forwardChan {
+			}
+			for range backwardChan {
+			}
+			if options.Verbose {
+				log.Printf("close connection from %s", localConn.LocalAddr().String())
+			}
+		}()
+
+	}
 }
