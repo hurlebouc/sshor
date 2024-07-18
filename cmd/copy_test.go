b1c1c7008c4ebf900b7ea925608265da20032c9a
hubert <hubert@hubert.com>
2024-07-18 05:58:46 +0200

    TU

--- cmd/copy_test.go
+++ cmd/copy_test.go
@@ -8,10 +8,10 @@ import (
 	"github.com/hurlebouc/sshor/cmd"
 )
 
-func TestCopy(t *testing.T) {
-	remoteDir := initTempDir(directoryLayout{})
+func TestCopyDirLocalToRemote(t *testing.T) {
+	destDir := initTempDir(directoryLayout{})
 	defer func() {
-		err := os.RemoveAll(remoteDir)
+		err := os.RemoveAll(destDir)
 		if err != nil {
 			panic(err)
 		}
@@ -37,16 +37,16 @@ func TestCopy(t *testing.T) {
 			},
 		},
 	}
-	localDir := initTempDir(srcLayout)
+	srcDir := initTempDir(srcLayout)
 	defer func() {
-		err := os.RemoveAll(localDir)
+		err := os.RemoveAll(srcDir)
 		if err != nil {
 			panic(err)
 		}
 	}()
 
 	c := make(chan struct{})
-	go startSftpServer(c, "toto", "totopwd", 2344, remoteDir)
+	go startSftpServer(c, "toto", "totopwd", 2344, destDir)
 	<-c
 
 	oldArgs := os.Args
@@ -54,7 +54,7 @@ func TestCopy(t *testing.T) {
 	os.Args = []string{
 		"sshor",
 		"copy",
-		localDir,
+		srcDir,
 		"toto@127.0.0.1:",
 		"-w",
 		"totopwd",
@@ -62,8 +62,68 @@ func TestCopy(t *testing.T) {
 		"2344",
 	}
 	cmd.Execute()
-	newRemoteLayout := readDirectory(filepath.Join(remoteDir, filepath.Base(localDir)))
-	if !equalDirs(newRemoteLayout, srcLayout) {
-		t.Fatalf("remote directory\n--> %+v\nis distinct from local directory\n--> %+v", newRemoteLayout, srcLayout)
+	copiedLayout := readDirectory(filepath.Join(destDir, filepath.Base(srcDir)))
+	if !equalDirs(copiedLayout, srcLayout) {
+		t.Fatalf("remote directory\n--> %+v\nis distinct from local directory\n--> %+v", copiedLayout, srcLayout)
+	}
+}
+
+func TestCopyDirRemoteToLocal(t *testing.T) {
+	destDir := initTempDir(directoryLayout{})
+	defer func() {
+		err := os.RemoveAll(destDir)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	srcLayout := directoryLayout{
+		files: map[string]file{
+			"test1": {
+				content: []byte("coucou"),
+			},
+			"test2": {
+				content: []byte("plop"),
+			},
+			"test3": {},
+		},
+		dirs: map[string]directoryLayout{
+			"emptydir": {},
+			"subdir": {
+				files: map[string]file{
+					"subtest": {
+						content: []byte("sub"),
+					},
+				},
+			},
+		},
+	}
+	srcDir := initTempDir(srcLayout)
+	defer func() {
+		err := os.RemoveAll(srcDir)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	c := make(chan struct{})
+	go startSftpServer(c, "toto", "totopwd", 2344, srcDir)
+	<-c
+
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+	os.Args = []string{
+		"sshor",
+		"copy",
+		"toto@127.0.0.1:",
+		destDir,
+		"-w",
+		"totopwd",
+		"-p",
+		"2344",
+	}
+	cmd.Execute()
+	copiedLayout := readDirectory(destDir)
+	if !equalDirs(copiedLayout, srcLayout) {
+		t.Fatalf("remote directory\n--> %+v\nis distinct from local directory\n--> %+v", copiedLayout, srcLayout)
 	}
 }
