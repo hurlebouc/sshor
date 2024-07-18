eb7a3d4bd3546c246b9e1640f7a088c8c3814d80
hubert <hubert@hubert.com>
2024-07-18 06:45:10 +0200

    TU

--- cmd/copy_test.go
+++ cmd/copy_test.go
@@ -393,3 +393,78 @@ func TestCopyDirRemoteToLocal(t *testing.T) {
 		t.Fatalf("final directory\n--> %+v\nis distinct from source directory\n--> %+v", copiedLayout, srcLayout)
 	}
 }
+
+func TestCopyDirRemoteToMissingLocal(t *testing.T) {
+	destDir := initTempDir(directoryLayout{})
+	defer func() {
+		err := os.RemoveAll(destDir)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	subdir := directoryLayout{
+		files: map[string]file{
+			"test1": {
+				content: []byte("coucou"),
+			},
+			"test2": {
+				content: []byte("plop"),
+			},
+			"test3": {},
+		},
+		dirs: map[string]directoryLayout{
+			"emptydir": {},
+			"subdir": {
+				files: map[string]file{
+					"subtest": {
+						content: []byte("sub"),
+					},
+				},
+			},
+		},
+	}
+	srcLayout := directoryLayout{
+		files: map[string]file{
+			"foo": {
+				content: []byte("it's a trap!"),
+			},
+		},
+		dirs: map[string]directoryLayout{
+			"sub": subdir,
+		},
+	}
+	srcDir := initTempDir(srcLayout)
+	defer func() {
+		err := os.RemoveAll(srcDir)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	expectedLayout := directoryLayout{
+		dirs: map[string]directoryLayout{
+			"plop": subdir,
+		},
+	}
+
+	c := make(chan struct{})
+	go startSftpServer(c, "toto", "totopwd", 2344, srcDir)
+	<-c
+
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+	os.Args = []string{
+		"sshor",
+		"copy",
+		"toto@127.0.0.1:sub",
+		filepath.Join(destDir, "plop"),
+		"-w",
+		"totopwd",
+		"-p",
+		"2344",
+	}
+	cmd.Execute()
+	copiedLayout := readDirectory(destDir)
+	if !equalDirs(copiedLayout, expectedLayout) {
+		t.Fatalf("final directory\n--> %+v\nis distinct from source directory\n--> %+v", copiedLayout, srcLayout)
+	}
+}
