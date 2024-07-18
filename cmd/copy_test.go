c161c381d0ef58a31dca3c57d188b168e6903185
hubert <hubert@hubert.com>
2024-07-18 06:53:44 +0200

    TU

--- cmd/copy_test.go
+++ cmd/copy_test.go
@@ -1,6 +1,7 @@
 package cmd_test
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -319,6 +320,77 @@ func TestCopyDirDotRemoteToLocal(t *testing.T) {
 	}
 }
 
+func TestCopyDirMissingRemoteToLocal(t *testing.T) {
+	destDir := initTempDir(directoryLayout{})
+	defer func() {
+		err := os.RemoveAll(destDir)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	subdir := directoryLayout{
+		files: map[string]file{
+			"test1": {
+				content: []byte("coucou"),
+			},
+			"test2": {
+				content: []byte("plop"),
+			},
+			"test3": {},
+		},
+		dirs: map[string]directoryLayout{
+			"emptydir": {},
+			"subdir": {
+				files: map[string]file{
+					"subtest": {
+						content: []byte("sub"),
+					},
+				},
+			},
+		},
+	}
+	srcLayout := directoryLayout{
+		files: map[string]file{
+			"foo": {
+				content: []byte("it's a trap!"),
+			},
+		},
+		dirs: map[string]directoryLayout{
+			"sub": subdir,
+		},
+	}
+	srcDir := initTempDir(srcLayout)
+	defer func() {
+		err := os.RemoveAll(srcDir)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	c := make(chan struct{})
+	go startSftpServer(c, "toto", "totopwd", 2344, srcDir)
+	<-c
+
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+	os.Args = []string{
+		"sshor",
+		"copy",
+		"toto@127.0.0.1:plop",
+		destDir,
+		"-w",
+		"totopwd",
+		"-p",
+		"2344",
+	}
+	defer func() {
+		if recover() != nil {
+			fmt.Println("All is normal")
+		}
+	}()
+	cmd.Execute()
+	t.Fatal("Should not be here!")
+}
 func TestCopyDirRemoteToLocal(t *testing.T) {
 	destDir := initTempDir(directoryLayout{})
 	defer func() {
