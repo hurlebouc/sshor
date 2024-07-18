fa896798002fba62ea12a31b40d99f7fc7bd6992
hubert <hubert@hubert.com>
2024-07-18 05:58:59 +0200

    remove unreachable code

--- ssh/copy/copy.go
+++ ssh/copy/copy.go
@@ -2,7 +2,6 @@ package copy
 
 import (
 	"log"
-	"os"
 	"path/filepath"
 
 	"github.com/hurlebouc/sshor/ssh"
@@ -69,93 +68,5 @@ func Copy(options ssh.Options, src, dst Endpoint) {
 		copyDir(options, src, CompleteDstPath(src, dst))
 	} else {
 		copyFile(options, src, CompleteDstPath(src, dst))
-	}
-}
-
-func CopyFileToRemote(client *sftp.Client, src, dst string) {
-
-	err := client.MkdirAll(filepath.Dir(dst))
-	if err != nil {
-		panic(err)
-	}
-
-	f, err := client.Create(dst)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	srcReader, err := os.Open(src)
-	if err != nil {
-		panic(err)
-	}
-	defer srcReader.Close()
-
-	_, err = f.ReadFrom(srcReader)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func CopyFileToLocal(client *sftp.Client, src, dst string) {
-
-	err := os.MkdirAll(filepath.Dir(dst), os.ModeDir)
-	if err != nil {
-		panic(err)
-	}
-
-	f, err := os.Create(dst)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	srcReader, err := client.Open(src)
-	if err != nil {
-		panic(err)
-	}
-	defer srcReader.Close()
-
-	_, err = f.ReadFrom(srcReader)
-	if err != nil {
-		panic(err)
-	}
-}
-
-// CopyDirToRemote(client, /A/a, /B/b) creates /B/b and copies content of /A/a into /B/b
-func CopyDirToRemote(client *sftp.Client, src, dst string) {
-	err := client.MkdirAll(dst)
-	if err != nil {
-		panic(err)
-	}
-	entries, err := os.ReadDir(src)
-	if err != nil {
-		panic(err)
-	}
-	for _, entry := range entries {
-		if entry.IsDir() {
-			CopyDirToRemote(client, filepath.Join(src, entry.Name()), dst+"/"+entry.Name())
-		} else {
-			CopyFileToRemote(client, filepath.Join(src, entry.Name()), dst+"/"+entry.Name())
-		}
-	}
-}
-
-// CopyDirToLocal(client, /A/a, /B/b) creates /B/b and copies content of /A/a into /B/b
-func CopyDirToLocal(client *sftp.Client, src, dst string) {
-	err := os.MkdirAll(dst, os.ModeDir)
-	if err != nil {
-		panic(err)
-	}
-	entries, err := client.ReadDir(src)
-	if err != nil {
-		panic(err)
-	}
-	for _, entry := range entries {
-		if entry.IsDir() {
-			CopyDirToLocal(client, src+"/"+entry.Name(), filepath.Join(dst, entry.Name()))
-		} else {
-			CopyFileToLocal(client, src+"/"+entry.Name(), filepath.Join(dst, entry.Name()))
-		}
 	}
 }
