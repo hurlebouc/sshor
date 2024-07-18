d7e734b5bdc5d6699855be62cfc51f49e2aaaf1c
hubert <hubert@hubert.com>
2024-07-18 06:36:35 +0200

    TU

--- cmd/copy_test.go
+++ cmd/copy_test.go
@@ -8,7 +8,74 @@ import (
 	"github.com/hurlebouc/sshor/cmd"
 )
 
-func TestCopyDirLocalToRemote(t *testing.T) {
+func TestCopyDirDotLocalToRemote(t *testing.T) {
+	destDir := initTempDir(directoryLayout{})
+	defer func() {
+		err := os.RemoveAll(destDir)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	srcLayout := directoryLayout{
+		files: map[string]file{
+			"test1": {
+				content: []byte("coucou"),
+			},
+			"test2": {
+				content: []byte("plop"),
+			},
+			"test3": {},
+		},
+		dirs: map[string]directoryLayout{
+			"emptydir": {},
+			"subdir": {
+				files: map[string]file{
+					"subtest": {
+						content: []byte("sub"),
+					},
+				},
+			},
+		},
+	}
+	srcDir := initTempDir(srcLayout)
+	defer func() {
+		err := os.RemoveAll(srcDir)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	c := make(chan struct{})
+	go startSftpServer(c, "toto", "totopwd", 2344, destDir)
+	<-c
+
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+	os.Args = []string{
+		"sshor",
+		"copy",
+		".",
+		"toto@127.0.0.1:",
+		"-w",
+		"totopwd",
+		"-p",
+		"2344",
+	}
+	oldwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	os.Chdir(srcDir)
+	defer os.Chdir(oldwd)
+
+	cmd.Execute()
+	copiedLayout := readDirectory(destDir)
+	if !equalDirs(copiedLayout, srcLayout) {
+		t.Fatalf("final directory\n--> %+v\nis distinct from source directory\n--> %+v", copiedLayout, srcLayout)
+	}
+}
+
+func TestCopyDirLocalToDotRemote(t *testing.T) {
 	destDir := initTempDir(directoryLayout{})
 	defer func() {
 		err := os.RemoveAll(destDir)
