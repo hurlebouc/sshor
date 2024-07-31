0677938c36e1b4511f616e54f9e94e11550460b3
hubert <hubert@hubert.com>
2024-07-31 08:14:48 +0200

    sign build output

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+set -e
+
 podman-compose build sshor
 container=$(podman create localhost/sshor:latest)
+
+rm -rf target/bin
 podman export "$container" | tar -xvf -
+
+for file in target/bin/*; do 
+    if [ -f "$file" ]; then 
+        gpg --detach-sign $file
+    fi 
+done
+
+
